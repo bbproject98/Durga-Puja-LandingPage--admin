c85277e2f3724b4fa67d5143080cadca9f7052c9
Sarkarparthainfomaticae <sakarpartha222@gmail.com>
2026-09-24 13:31:30 +0530

    lay out new

--- Broomboom landing page/app/layout.tsx
+++ Broomboom landing page/app/layout.tsx
@@ -80,39 +80,57 @@ export default function RootLayout({
         />
       </head>
       <body className="font-sans bg-[#FFFDF7] text-slate-900 antialiased selection:bg-amber-500 selection:text-slate-950">
-          <Script
-    id="google-tag-manager"
-    strategy="afterInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-N6D7HH8');
-      `,
-    }}
-  />
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N6D7HH8');
+            `,
+          }}
+        />
 
-  {/* Google Analytics 4 */}
-  <Script
-    src="https://www.googletagmanager.com/gtag/js?id=G-JD89KXBSS2"
-    strategy="afterInteractive"
-  />
+        {/* Google Analytics 4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JD89KXBSS2"
+          strategy="afterInteractive"
+        />
 
-  <Script
-    id="google-analytics"
-    strategy="afterInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-JD89KXBSS2');
-      `,
-    }}
-  />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JD89KXBSS2');
+            `,
+          }}
+        />
+
+        {/* Main Application Content */}
         {children}
+
+        {/* Floating Call Button - Optimized for Mobile & Desktop */}
+        <a
+          href="tel:+918240765499"
+          className="fixed bottom-20 right-2 sm:bottom-6 sm:right-6 z-50 flex h-12 w-12 sm:h-14 sm:w-14 items-center justify-center rounded-full bg-amber-500 text-slate-950 shadow-2xl transition-all duration-300 hover:scale-110 hover:bg-amber-400 focus:outline-none"
+          aria-label="Call BroomBoom Cabs"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="h-5 w-5 sm:h-6 sm:w-6"
+          >
+            <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
+          </svg>
+        </a>
       </body>
     </html>
   );
